b428a10f4c0c52cf7542205d1a9c66baeb538d70
trhongbinwang <trhongbinwang@gamil.com>
2017-04-18 22:54:13 +0100

    updating

--- deep_learning/keras/examples/mnist_cnn.py
+++ deep_learning/keras/examples/mnist_cnn.py
@@ -13,7 +13,7 @@ from tensorflow.contrib.keras.python.keras.models import Model
 from tensorflow.contrib.keras.python.keras.layers import Input, Dense, Dropout, Flatten
 from tensorflow.contrib.keras.python.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.contrib.keras.python.keras import backend as K
-from tensorflow.contrib.keras.python.keras.objectives import categorical_crossentropy
+from tensorflow.contrib.keras.python.keras.losses import categorical_crossentropy
 
 
 # add prefix in import line. from tensorflow.contrib.keras.python.
@@ -64,20 +64,26 @@ def inputs_placeholder():
     return [img, labels]
 
 
-def cnn_model_tf(img, labels):
+def cnn_model_tf(inputs):
     '''
     defin the model in tf way
 
     '''
-    x = Conv2D(32, kernel_size=(3, 3), activation='relu')(inputs)
+    img, labels = inputs[0], inputs[1]
+    x = Conv2D(32, kernel_size=(3, 3), activation='relu')(img)
     x = Conv2D(64, (3, 3), activation='relu')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Dropout(0.25)(x)
+    #x = Dropout(0.25)(x)
     x = Flatten()(x)
     x = Dense(128, activation='relu')(x)
-    x = Dropout(0.5)(x)
+    #x = Dropout(0.5)(x)
     pred = Dense(num_classes, activation='softmax')(x)
     loss = tf.reduce_mean(categorical_crossentropy(labels, pred))
+    # train
+    train_op = tf.train.AdadeltaOptimizer().minimize(loss)
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(labels, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+    return [train_op, accuracy]
 
 
 def cnn_model_fn():
@@ -102,6 +108,7 @@ def cnn_model_fn():
                   metrics=['accuracy'])
     return model
 
+
 def train(data, model):
     '''
     train the model
@@ -118,6 +125,29 @@ def train(data, model):
     print('Test accuracy:', score[1])
 
 
+def train_tf(sess, data, inputs, model):
+    '''
+
+    '''
+    x_train, y_train, x_test, y_test = data[0], data[1], data[2], data[3]
+    img, labels = inputs[0], inputs[1]
+    train_op, accuracy = model[0], model[1]
+
+    for epoch_i in range(epochs):
+        print(epoch_i)
+        # in a loop, create training_batch, feed to train_op
+        for i in range(10):
+            training_batch = zip(range(0, len(x_train), batch_size),
+                                 range(batch_size, len(x_train) + 1, batch_size))
+            for start, end in training_batch:
+                sess.run(train_op, feed_dict={img: x_train[start:end], labels: y_train[start:end]})
+            print(sess.run(accuracy,
+                           feed_dict={
+                               img: x_test[:100],
+                               labels: y_test[:100],
+                           }))
+
+
 def main_tf():
     '''
     main func in tf way
@@ -125,6 +155,15 @@ def main_tf():
     # initialiaze tf session
     sess = tf.Session()
     K.set_session(sess)  # register tf session with keras
+    # load data
+    data = load_data()
+    # define inputs
+    inputs = inputs_placeholder()
+    # define model
+    model = cnn_model_tf(inputs)
+    # train
+    sess.run(tf.global_variables_initializer())
+    train_tf(sess, data, inputs, model)
 
 
 def main_fn():
@@ -136,8 +175,10 @@ def main_fn():
     model = cnn_model_fn()
     train(data, model)
 
+
 if __name__ == '__main__':
     '''
 
     '''
-    main_fn()
+#    main_fn()
+    main_tf()
